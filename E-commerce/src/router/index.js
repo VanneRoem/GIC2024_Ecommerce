b9c1149b7afaecfe0066b28ff39465f3f401c543
VanneRoem <vannel572@gmail.com>
2024-11-28 08:26:26 +0700

    Finish TP4 Step 02 Move code from App.vue to HomeView.vue Add RouterView to App.vue

--- E-commerce/src/router/index.js
+++ E-commerce/src/router/index.js
@@ -1,10 +1,28 @@
+import Category from '@/components/Category.vue'
+import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+  { 
+    Path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/categories/catagoryId",
+    name: "category",
+    component: () => import("../views/CategoryView.vue"),
+  },
+  {
+    path: "/products/:productId",
+    name: "product",
+    component:() => import("../views/ProductView.vue"),
+  },
     
   ]
 })
+
 
 export default router
